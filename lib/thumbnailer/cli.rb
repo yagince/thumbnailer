f9c02ac2e84db93991035cffd435851e122f6dd7
yagince <natsuki.yagi@viibar.com>
2015-12-28 15:24:48 +0900

    add video and thumbnail format

--- lib/thumbnailer/cli.rb
+++ lib/thumbnailer/cli.rb
@@ -7,12 +7,16 @@ require 'string_ext'
 class Thumbnailer::CLI < Thor
 
   desc 'gen MOVIE_DIR', 'generate thumbnails'
-  option :size,            type: :string,  required: true, default: '500x282',    aliases: '-s', desc: 'size of thumbnail. "{width}x{height}" ex) 125x70'
-  option :output_dir,      type: :string,  required: true, default: 'thumbnails', aliases: '-o', desc: 'output directory path'
-  option :thumbnail_count, type: :numeric, required: true, default: 16,           aliases: '-n', desc: 'number of thumbnails'
+  option :size,            type: :string,  required: true, default: '500x282',    aliases: '-s',  desc: 'size of thumbnail. "{width}x{height}" ex) 125x70'
+  option :output_dir,      type: :string,  required: true, default: 'thumbnails', aliases: '-o',  desc: 'output directory path'
+  option :thumbnail_count, type: :numeric, required: true, default: 16,           aliases: '-n',  desc: 'number of thumbnails'
+  option :thumbnail_format,type: :string,  required: true, default: 'png',                        desc: 'format of thubnail file ex) png, jpg'
+  option :video_format,    type: :string,  required: true, default: 'mp4',                        desc: 'format of video file ex) mp4'
   def gen(movie_dir='./')
-    Dir.glob(File.join(movie_dir, '**/*.mp4')) do |path|
-      thumbnail_dir = "#{options[:output_dir]}/#{File.basename(path).gsub('.mp4', '')}"
+    video_format = options[:video_format]
+
+    Dir.glob(File.join(movie_dir, "**/*.#{video_format}")) do |path|
+      thumbnail_dir = "#{options[:output_dir]}/#{File.basename(path).gsub('.'+video_format, '')}"
       count = options[:thumbnail_count]
       puts "[START] generate thumbnail into #{thumbnail_dir} (#{count} thumbnails)".green
 
@@ -24,7 +28,7 @@ class Thumbnailer::CLI < Thor
       count.times { |i|
         seek_time = i * interval
         if seek_time < movie.duration.to_i
-          movie.screenshot("#{thumbnail_dir}/#{i}.png", { seek_time: seek_time, resolution: options[:size] }, preserve_aspect_ratio: :width)
+          movie.screenshot("#{thumbnail_dir}/#{i}.#{options[:thumbnail_format]}", { seek_time: seek_time, resolution: options[:size] }, preserve_aspect_ratio: :width)
         else
           puts "[ERROR] no thumbnail at #{seek_time} second.".pink
         end
