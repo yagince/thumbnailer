3a382c8b1c89a6bd0c57c75b1761c52f6ca21f2b
yagince <natsuki.yagi@viibar.com>
2015-12-28 13:22:03 +0900

    サムネイル数を引数に

--- lib/thumbnailer/cli.rb
+++ lib/thumbnailer/cli.rb
@@ -2,24 +2,31 @@ require 'fileutils'
 require 'thor'
 require 'streamio-ffmpeg'
 
+require 'string_ext'
+
 class Thumbnailer::CLI < Thor
 
   desc 'gen MOVIE_DIR', 'generate thumbnails'
-  option :size,       type: :string, required: true, default: '500x282',    aliases: '-s', desc: 'size of thumbnail. "{width}x{height}" ex) 125x70'
-  option :output_dir, type: :string, required: true, default: 'thumbnails', aliases: '-o', desc: 'output directory path'
+  option :size,            type: :string,  required: true, default: '500x282',    aliases: '-s', desc: 'size of thumbnail. "{width}x{height}" ex) 125x70'
+  option :output_dir,      type: :string,  required: true, default: 'thumbnails', aliases: '-o', desc: 'output directory path'
+  option :thumbnail_count, type: :numeric, required: true, default: 16,           aliases: '-n', desc: 'number of thumbnails'
   def gen(movie_dir='./')
     Dir.glob(File.join(movie_dir, '**/*.mp4')) do |path|
       thumbnail_dir = "#{options[:output_dir]}/#{File.basename(path).gsub('.mp4', '')}"
+      count = options[:thumbnail_count]
+      puts "[START] generate thumbnail into #{thumbnail_dir} (#{count} thumbnails)".green
 
       cleanup_dir thumbnail_dir
 
       movie = FFMPEG::Movie.new(path)
-      interval = (movie.duration / 16).to_i
-      interval = [interval, 1].max
+      interval = interval_seq movie, count
 
-      (0..1).each{|i|
-        if i * interval < (movie.duration - 0.5)
-          movie.screenshot("#{thumbnail_dir}/#{i}.png", { seek_time: i * interval, resolution: options[:size] }, preserve_aspect_ratio: :width)
+      count.times { |i|
+        seek_time = i * interval
+        if seek_time < movie.duration.to_i
+          movie.screenshot("#{thumbnail_dir}/#{i}.png", { seek_time: seek_time, resolution: options[:size] }, preserve_aspect_ratio: :width)
+        else
+          puts "[ERROR] no thumbnail at #{seek_time} second.".pink
         end
       }
     end
@@ -29,6 +36,10 @@ class Thumbnailer::CLI < Thor
     def cleanup_dir path
       FileUtils.rm_r path if File.exists? path
       FileUtils.mkdir_p path
+    end
+
+    def interval_seq movie, count
+      [(movie.duration / count).to_i, 1].max
     end
   end
 end
